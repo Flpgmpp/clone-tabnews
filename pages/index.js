19dc9b3d966ef4a37f48c0762d1915743dc75a9a
Flpgmpp <felipegempp2015@gmail.com>
2025-11-22 23:40:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-function Home () {
-    return <h1>Tab news, o clone finalmente está saindo xD</h1>
+function Home() {
+  return <h1>Tab news, o clone finalmente está saindo xD</h1>;
+}
+function teste() {
+  console.log("testando o prettier");
+}
+function teste2() {
+  console.log("identação errada propositalmente");
 }
 export default Home;
