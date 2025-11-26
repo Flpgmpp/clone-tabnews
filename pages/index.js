931c46b953af735e2077eb19572b5d1be0d89cae
Flpgmpp <felipegempp2015@gmail.com>
2025-11-26 16:01:22 +0000

    adiciona `.prettierignore.`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
   return <h1>Tab news, o clone finalmente está saindo xD</h1>;
 }
-function teste() {
-  console.log("testando o prettier");
-}
-function teste2() {
-  console.log("identação errada propositalmente");
-}
+
 export default Home;
